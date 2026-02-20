434c12d1fcba69cc3e8c5f432025d04dbae84104
K Ramesh <krameshr348@gmail.com>
2026-02-20 14:45:23 +0530

    save

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 
 const App = () => {
   return (
-    <main>
-      <Navbar />
-      <div className="container d-flex justify-content-center align-items-center min-vh-100 py-5">
-        <div style={{ width: "100%", maxWidth: "500px" }}>
-          <Home />
+    <BrowserRouter>
+      <main>
+        <Navbar />
+        <div className="container d-flex justify-content-center align-items-center min-vh-100 py-5">
+          <div style={{ width: "100%", maxWidth: "500px" }}>
+            <Home />
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </BrowserRouter>
   );
 };
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,49 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom shadow-sm sticky-top">
-        <div className="container">
-          <a className="navbar-brand text-primary fw-bold fs-3" href="#">
-            FormS
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav ms-auto">
-              <a
-                className="nav-link active fw-semibold px-3"
-                aria-current="page"
-                href="#"
-                style={{ color: "#0d6efd" }}
-              >
-                Home
-              </a>
-              <a className="nav-link px-3 text-dark" href="#">
-                Features
-              </a>
-              <a className="nav-link px-3 text-dark" href="#">
-                Pricing
-              </a>
-              <a className="nav-link px-3 text-dark" href="#">
-                About
-              </a>
-            </div>
-            <button className="btn btn-primary ms-3 px-4">Get Started</button>
+    <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom shadow-sm sticky-top">
+      <div className="container">
+        <Link className="navbar-brand text-primary fw-bold fs-3" to="/">
+          FormS
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto">
+            <Link
+              className="nav-link active fw-semibold px-3"
+              to="/"
+              style={{ color: "#0d6efd" }}
+            >
+              Home
+            </Link>
+
+            <Link className="nav-link px-3 text-dark" to="/features">
+              Features
+            </Link>
+
+            <Link className="nav-link px-3 text-dark" to="/pricing">
+              Pricing
+            </Link>
+
+            <Link className="nav-link px-3 text-dark" to="/about">
+              About
+            </Link>
           </div>
+
+          <button className="btn btn-primary ms-3 px-4">Get Started</button>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
